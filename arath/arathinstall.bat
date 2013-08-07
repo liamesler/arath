cb7931b231a4eca4440602fb4f3f07dcb20361cd
Liam Esler <liam.esler@gmail.com>
2013-08-07 13:49:39 +1000

    One way to fix the tis error once and for all :P

--- arath/arathinstall.bat
+++ arath/arathinstall.bat
@@ -6,12 +6,3 @@ del override\lk#ar*.ogg
 del override\lk#blk.mus
     
 @echo Finished installing audio.
-
-@echo Installing areas...
-
-arath\tisunpack.exe -s override\lk#ar1.tiz
-arath\tisunpack.exe -s override\lk#ar2.tiz
-
-del override\lk#ar*.tiz
-
-@echo Finished installing areas.
--- arath/arathuninstall.bat
+++ arath/arathuninstall.bat
@@ -3,10 +3,3 @@
     
 del override\lk#ar*.wav
 @echo Finished uninstalling audio.
-
-@echo Uninstalling areas...
-
-del override\lk#ar1.tis
-del override\lk#ar2.tis
-
-@echo Finished uninstalling areas.
